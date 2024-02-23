854678d183b948a7ac93b665b622de732a362d9e
tjordan4 <tysoncjordan@gmail.com>
2024-02-23 11:27:42 -0700

    completed router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,9 +8,17 @@ function App() {
   return (
     <>
       <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <h1></h1>
+      <div id="navbar">
+        <link href="" />
+        <link href="" />
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<h1>Blue</h1>}></Route>
+          <Route path="/red" element={<h1>Red</h1>}></Route>
+        </Routes>
+      </div>
     </div>
     </>
   )
