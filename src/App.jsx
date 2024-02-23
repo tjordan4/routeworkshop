2edcaea75bb6518a106b1f5098e53091a6b9aab8
tjordan4 <tysoncjordan@gmail.com>
2024-02-23 11:48:23 -0700

    finished workshop

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Routes, Route, Link } from 'react-router-dom'
 import Red from './components/Red'
 import Blue from './components/Blue'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,13 +13,15 @@ function App() {
       <div id="container">
       <h1></h1>
       <div id="navbar">
-        <Link to="/blue">Blue</Link>;
-        <Link to="/red">Red</Link>;
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/home">Home</Link>
         </div>
       <div id="main-section">
         <Routes>
           <Route path="/blue" element={<Blue/>}></Route>
           <Route path="/red" element={<Red/>}></Route>
+          <Route path="/home" element={<Home/>}></Route>
         </Routes>
       </div>
     </div>
